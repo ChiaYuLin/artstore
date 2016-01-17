bcb40f89213cd91db017ea922fb9a4bc9e563304
Chia-Yu Lin <sallylin0121@gmail.com>
2016-01-17 15:37:54 +0800

    If the quantity is 0, the customer cannot buy.

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -15,7 +15,11 @@ before_action :authenticate_user!
     @cart = current_cart
     @item = @cart.cart_items.find_by(product_id: params[:id])
 
-    @item.update(item_params)
+    if @item.product.quantity >= item_params[:quantity].to_i
+      @item.update(item_params)
+      flash[:notice] = "成功變更數量"
+    else
+      flash[:warning] = "數量不足以加入購物車"
 
     redirect_to carts_path
   end
